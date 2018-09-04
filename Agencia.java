63a6af1c29135e84aa3a421c9cf26dd71ea6f2f3
henriquesan14 <33522361+henriquesan14@users.noreply.github.com>
2018-09-04 15:01:10 -0300

    Add files via upload

--- Agencia.java
+++ Agencia.java
@@ -52,26 +52,39 @@ public class Agencia {
 		int opc = entrada.nextInt();
 		switch(opc){
 			case 1:System.out.println("Qual seu nome? ");
-				String nome=entrada.next();
+				String nome=entrada.nextLine();
+				entrada.nextLine();
+				
 				System.out.println("Qual sua renda? ");
 				double renda=entrada.nextDouble();
+				
 				System.out.println("Digite seu endereço: ");
 				System.out.println("Rua: ");
 				String rua=entrada.next();
+				entrada.nextLine();
 				System.out.println("Número: ");
 				int numero=entrada.nextInt();
+				
 				System.out.println("Bairro:  ");
 				String bairro=entrada.next();
+				entrada.nextLine();
+				
 				System.out.println("Cidade: ");
 				String cidade=entrada.next();
+				entrada.nextLine();
+				
 				System.out.println("CEP: ");
 				long cep=entrada.nextLong();
+				
 				System.out.println("Digite seu CPF: ");
 				long cpf=entrada.nextLong();
+				
 				System.out.println("Digite seu RG: ");
 				long rg=entrada.nextLong();
+				
 				System.out.println("Digite uma senha para sua conta: ");
 				String senha=entrada.next();
+				entrada.nextLine();
 				
 				Endereco endereco=new Endereco(rua,numero,bairro,cidade,cep);
 				PessoaFisica pf = new PessoaFisica(nome,renda,1,endereco,cpf,rg);
@@ -88,23 +101,36 @@ public class Agencia {
 				break;
 			case 2:System.out.println("Qual seu nome? ");
 				String nome2=entrada.next();
+				entrada.nextLine();
+				
 				System.out.println("Qual sua renda? ");
 				double renda2=entrada.nextDouble();
+				
 				System.out.println("Digite seu endereço: ");
 				System.out.println("Rua: ");
 				String rua2=entrada.next();
+				entrada.nextLine();
+				
 				System.out.println("Número: ");
 				int numero2=entrada.nextInt();
+				
 				System.out.println("Bairro:  ");
 				String bairro2=entrada.next();
+				entrada.nextLine();
+				
 				System.out.println("Cidade: ");
 				String cidade2=entrada.next();
+				entrada.nextLine();
+				
 				System.out.println("CEP: ");
 				long cep2=entrada.nextLong();
+				
 				System.out.println("Digite seu CNPJ: ");
 				long cnpj=entrada.nextLong();
+				
 				System.out.println("Digite uma senha para sua conta: ");
 				String senha2=entrada.next();
+				entrada.nextLine();
 				
 				Endereco endereco2=new Endereco(rua2,numero2,bairro2,cidade2,cep2);
 				PessoaJuridica pj = new PessoaJuridica(nome2,renda2,1,endereco2,cnpj);
